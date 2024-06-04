db3aa546d73eeecefda2be098dbf34f1b5e0ca51
Zet <pablowt83@gmail.com>
2024-06-04 09:17:22 -0600

    Development Start

---
+++ client/full_stack_front/src/views/HomeView.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-card>
+    <v-app>
+      <v-navigation-drawer
+        v-model="drawer"
+        :rail="rail"
+        permanent
+        @click="rail = false"
+      >
+        <v-list-item
+          prepend-avatar="https://randomuser.me/api/portraits/men/85.jpg"
+          title="John Leider"
+          nav
+        >
+          <template v-slot:append>
+            <v-btn
+              icon="mdi-chevron-left"
+              variant="text"
+              @click.stop="rail = !rail"
+            ></v-btn>
+          </template>
+        </v-list-item>
+
+        <v-divider></v-divider>
+
+        <v-list density="compact" nav>
+          <v-list-item prepend-icon="mdi-home-city" title="Home" value="home"></v-list-item>
+          <v-list-item prepend-icon="mdi-account" title="My Account" value="account"></v-list-item>
+          <v-list-item prepend-icon="mdi-cart" title="Shopping" value="Shopping"></v-list-item>
+          <v-list-item prepend-icon="mdi-information" title="About Us" value="About Us"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+      <v-main style="height: 250px"></v-main>
+    </v-app>
+  </v-card>
+</template>
+
+<script>
+
+  export default {
+    data () {
+      return {
+        drawer: true,
+        rail: true,
+      }
+    },
+  }
+</script>
+
