990d6d666ec47b4f87ab1c8458dbcdc949b05d16
Zet <pablowt83@gmail.com>
2024-06-04 12:15:20 -0600

    home view edited
    added some changes to the general UI

--- client/full_stack_front/src/views/HomeView.vue
+++ client/full_stack_front/src/views/HomeView.vue
@@ -1,6 +1,19 @@
 <template>
   <v-card>
     <v-app>
+      <v-app-bar :elevation="2">
+        <template>
+            <v-app-bar-nav-icon></v-app-bar-nav-icon>  
+        </template>
+
+        <v-app-bar-title>Tienda FullStack</v-app-bar-title>
+
+        <template v-slot:append>
+          <v-btn icon="mdi-search"></v-btn>
+          <v-btn icon ="mdi-cart"></v-btn>
+        </template>
+      </v-app-bar>
+
       <v-navigation-drawer
         v-model="drawer"
         :rail="rail"
@@ -30,7 +43,25 @@
           <v-list-item prepend-icon="mdi-information" title="About Us" value="About Us"></v-list-item>
         </v-list>
       </v-navigation-drawer>
-      <v-main style="height: 250px"></v-main>
+      
+      <v-main style="height: 250px">
+        <v-carousel show-arrows ="hover" height="15rem">
+          <v-carousel-item src="../assets/imgs/Slide1.png" cover></v-carousel-item>
+          <v-carousel-item src="../assets/imgs/Slide2.png" cover></v-carousel-item>
+          <v-carousel-item src="../assets/imgs/Slide3.png" cover></v-carousel-item>
+        </v-carousel>
+
+        <v-container class="mb-6">
+          <v-row align-center style="height: 15rem;" no-gutters>
+          <v-col v-for="n in 3" :key="n">
+            <v-sheet class="pa-2 ma-2">
+              <v-img cover src="../assets/imgs/Slide1.png"></v-img>
+              <p class="text-center">primero</p>
+            </v-sheet>
+          </v-col>
+        </v-row>
+        </v-container>
+      </v-main>
     </v-app>
   </v-card>
 </template>
